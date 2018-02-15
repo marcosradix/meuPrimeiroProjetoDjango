5884888378b95d5717fe83d6a0472921d99a2463
marcosradix <marcosradix@gmail.com>
2018-02-15 17:14:23 -0300

    urls

--- django001/clientes/views.py
+++ django001/clientes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Person
 # Create your views here.
 def lista_pessoa(request):
-    return render(request, 'pessoa.html')
+    persons = Person.objects.all()
+    return render(request, 'person.html', {'objectPerson': persons})
